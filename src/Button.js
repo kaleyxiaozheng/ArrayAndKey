34d983f5488a72bef950a017c17678a4327ba254
kaleyxiaozheng <zheng.kaley.x@gmail.com>
2020-06-16 15:13:56 +1000

    change component

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-export const Button = (props)=> {
-
-        return (
-            <div >
-                <button onClick={props.onClick} disabled={props.checkValue}>{props.label}</button>
-            </div>
-        );
-}
+export const Button = (props) => (
+  <div >
+    <button onClick={props.onClick} disabled={props.checkValue}>{props.label}</button>
+  </div>
+)
--- src/Form.js
+++ src/Form.js
@@ -11,27 +11,22 @@ export class Form extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({ number: parseInt(event.target.value) });
     }
 
-    // handleSubmit(event) {
-    //     event.preventDefault();
-    // }
-
     render() {
         return (
             <div>
-                    <div className="lableForm">
-                        <label>Input Number</label>
-                    </div>
-                    <div >
-                        <input className="inputForm" type="text" value={this.state.number} onChange={this.handleChange} />
-                    </div>
-                    <Button onClick={() => this.props.onClick(this.state.number)} label="submit"></Button>
+                <div className="lableForm">
+                    <label>Input Number</label>
+                </div>
+                <div >
+                    <input className="inputForm" type="text" value={this.state.number} onChange={this.handleChange} />
+                </div>
+                <Button onClick={() => this.props.onClick(this.state.number)} label="submit"></Button>
             </div>
         );
     }
